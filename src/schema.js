588d54b0c819fb463e7e997b67672c49d61f8709
mist635 <magurisu0605@gmail.com>
2025-05-11 04:49:17 +0900

    Update schema.js

--- src/schema.js
+++ src/schema.js
@@ -1,11 +1,11 @@
 // src/schema.js
 export const schema = {
-    title: "プロフィール",
-    type: "object",
-    properties: {
-      name: { type: "string", title: "名前" },
-      age: { type: "number", title: "年齢" }
-    },
-    required: ["name", "age"]
-  };
-  
+  title: "プロフィール",
+  type: "object",
+  properties: {
+    name: { type: "string", title: "名前" },
+    age: { type: "number", title: "年齢" }
+  },
+  required: ["name", "age"]
+};
+export { schemaDefinition };  
